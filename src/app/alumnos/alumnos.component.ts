454c1e0d193d1c0449ab6da894a32de9d728e26f
Leobardo Daniel Vargas <ldvr30345@gmail.com>
2025-05-14 13:36:21 -0500

    feat: implementar formulario reactivo en el componente Alumnos con validaciones y suscripciones a cambios de valor

--- src/app/alumnos/alumnos.component.ts
+++ src/app/alumnos/alumnos.component.ts
@@ -1,33 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder,ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alumnos',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './alumnos.component.html',
   styleUrl: './alumnos.component.css'
 })
-export class AlumnosComponent implements OnInit{
+export class AlumnosComponent implements OnInit {
 
   searchForm: FormGroup;
   opcionSeleccionada: any;
   constructor(private fb: FormBuilder) {
+
     this.searchForm = this.fb.group({
-      filterSearch: ['', Validators.required]
+      filterSearch: ['', Validators.required],
+      searchInput: ['', Validators.required]
     });
   }
   ngOnInit(): void {
-    this.searchForm.get('seleccion')?.valueChanges.subscribe(valor => {
-      this.opcionSeleccionada =this.opcionSeleccionada.id;
-      console.log('Opción seleccionada:', this.opcionSeleccionada);
+    this.searchForm.get('filterSearch')?.valueChanges.subscribe(valor => {
+      //this.opcionSeleccionada = this.opcionSeleccionada.id;
+      console.log('Opción seleccionada:', valor);
     });
 
-        this.searchForm.get('campoTexto')?.valueChanges.subscribe(valor => {
-          console.log('Campo de texto cambiado:', valor);
-          
+    this.searchForm.get('searchInput')?.valueChanges.subscribe(valor => {
+      console.log('Campo de texto cambiado:', valor);
+
     });
 
   }
-  
+
 
 }
