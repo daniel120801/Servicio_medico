c6392f5be7f1939e4e1d14efe955acbdfa02b355
Tania2809 <angel24torrest@gmail.com>
2025-05-15 10:23:39 -0500

    feat: add form modal components for conference and registration with corresponding templates and styles

---
+++ src/app/form-registro-modal/form-registro-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-registro-modal',
+  imports: [],
+  templateUrl: './form-registro-modal.component.html',
+  styleUrl: './form-registro-modal.component.css'
+})
+export class FormRegistroModalComponent {
+
+}
