4f6114e0c4ea0202f59f534ec180f5a723c24dd5
Tania2809 <angel24torrest@gmail.com>
2025-05-19 08:46:29 -0500

    feat: add form modal components for consulta and vacunas with corresponding templates, styles, and tests

---
+++ src/app/form-vacunas-modal/form-vacunas-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-vacunas-modal',
+  imports: [],
+  templateUrl: './form-vacunas-modal.component.html',
+  styleUrl: './form-vacunas-modal.component.css'
+})
+export class FormVacunasModalComponent {
+
+}
