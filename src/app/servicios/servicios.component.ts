a74ed12694ada7e69a2a9ebc07aaf41403be227c
Tania2809 <angel24torrest@gmail.com>
2025-05-27 12:42:37 -0500

    feat: add QR code functionality and improve vacunas component layout

--- src/app/servicios/servicios.component.ts
+++ src/app/servicios/servicios.component.ts
@@ -2,15 +2,37 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // <-- Agrega esto
 import { FormConsultaModalComponent } from "../form-consulta-modal/form-consulta-modal.component";
 import { FormVacunasModalComponent } from "../form-vacunas-modal/form-vacunas-modal.component";
+import { VacunasComponent } from "../vacunas/vacunas.component";
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
   styleUrls: ['./servicios.component.css'],
   standalone: true,
-  imports: [CommonModule, FormConsultaModalComponent, FormVacunasModalComponent] // <-- Agrega CommonModule aquí
+  imports: [CommonModule, FormConsultaModalComponent, FormVacunasModalComponent, VacunasComponent] // <-- Agrega CommonModule aquí
+ // <-- Agrega CommonModule aquí
 })
 export class ServiciosComponent {
+vacunas: string[] =[
+  'Vacuna A',
+  'Vacuna B',
+  'Vacuna C',
+  'Vacuna D'
+];
+
+  selectedVacuna: string | null = null;
+
+showVacunas = false;
+
+onVacunaSeleccionada(vacuna: any) {
+  this.selectedVacuna = vacuna;
+    console.log('Vacuna seleccionada:', this.selectedVacuna);
+  this.showVacunas = true;
+}
+
+
+
+  
   formularioConsultaVisible = false;
   formularioVacunasVisible = false;
 
