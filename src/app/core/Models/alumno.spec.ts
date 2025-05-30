4101b92ecd51e925ede31b7f08238bb0e0fab158
Leobardo Daniel Vargas <ldvr30345@gmail.com>
2025-05-30 12:03:07 -0500

    feat: eliminar archivos innecesarios y agregar campo de carrera en el perfil del alumno

--- src/app/core/Models/alumno.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Alumno } from '../../Alumnos/models/alumno.model';
-
-describe('Alumno', () => {
-  it('should create an instance', () => {
-    expect(new Alumno()).toBeTruthy();
-  });
-});
